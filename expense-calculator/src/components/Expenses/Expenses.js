14dba4a75241ef543ef010ff602b08a8d1ada458
deepangunasekaran <71061219+deepangunasekaran@users.noreply.github.com>
2022-01-24 14:35:39 +0530

    Create Expenses.js

---
+++ expense-calculator/src/components/Expenses/Expenses.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  if (props.items.length === 0) {
+    return (
+      <Card className="expenses">
+        <h1 style={{ color: "white" }}>NO DATA</h1>
+      </Card>
+    );
+  } else {
+    return (
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            key={expense.id}
+          />
+        ))}
+      </Card>
+    );
+  }
+}
+
+export default Expenses;
